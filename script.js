73f8211b2c6b1df719276892779e2d5567d0ef7d
Vindeshwari Prasad <vinayguptasia53@gmail.com>
2024-03-26 05:16:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-function firstWord(s) {
+function firstWord(str) {
   // your code here
+	if (!str || str.trim().length === 0) {
+        return str;
+    }
+    
+    // Split the string by space and return the first element
+    return str.trim().split(' ')[0];
 }
 
 // Do not change the code below
